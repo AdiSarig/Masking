34acf90880b461524680debd783bb48646b01767
AdiSarig <74588101+AdiSarig@users.noreply.github.com>
2021-05-05 19:59:10 +0300

    resize probe

--- dispProbe.m
+++ dispProbe.m
@@ -2,11 +2,11 @@ function dispProbe(session)
 
     % [xCenter, yCenter] = RectCenter(session.windowRect);
     dotColor = [1 0 0];
-    dotSizePix = session.window/8;
     
     screenXpixels = session.windowRect(3) - session.windowRect(1);
     screenYpixels = session.windowRect(4) - session.windowRect(2);
     rand('seed', sum(100 * clock));
+dotSizePix = 20;
 
     dotXpos = rand * screenXpixels;
     dotYpos = rand * screenYpixels;
