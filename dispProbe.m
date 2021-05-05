4c1dbba75f58ced5527a8ebc81bcb6c6ac3d82f7
AdiSarig <74588101+AdiSarig@users.noreply.github.com>
2021-05-05 20:00:26 +0300

    Adjust probe location
    Stimuli location is changed to be part of the screen and the probe should be displayed on top of it.

--- dispProbe.m
+++ dispProbe.m
@@ -1,16 +1,19 @@
-function dispProbe(session)
+function xy_loc = dispProbe(session, xy_loc)
 
-    % [xCenter, yCenter] = RectCenter(session.windowRect);
-    dotColor = [1 0 0];
-    
-    screenXpixels = session.windowRect(3) - session.windowRect(1);
-    screenYpixels = session.windowRect(4) - session.windowRect(2);
-    rand('seed', sum(100 * clock));
+% [xCenter, yCenter] = RectCenter(session.windowRect);
+dotColor = [1 0 0];
 dotSizePix = 20;
 
-    dotXpos = rand * screenXpixels;
-    dotYpos = rand * screenYpixels;
+if isempty(xy_loc)
+    screenXpixels = session.stim.location(3) - session.stim.location(1) - dotSizePix;
+    screenYpixels = session.stim.location(4) - session.stim.location(2) - dotSizePix;
+    rand('seed', sum(100 * clock));
+    
+    dotXpos = rand * screenXpixels + session.stim.location(1);
+    dotYpos = rand * screenYpixels + session.stim.location(2);
+    xy_loc = [dotXpos dotYpos] + dotSizePix/2;
+end
 
-    Screen('DrawDots', session.window, [dotXpos dotYpos], dotSizePix, dotColor, [], 2);
+Screen('DrawDots', session.window, xy_loc, dotSizePix, dotColor, [], 2);
 
 end
