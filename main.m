a6df6dc13be2e3a3032f9ecf4f3e1abbf7d3bb45
Uri <uri.korisky@gmail.com>
2021-05-09 15:07:54 +0300

    initialize vpixx

--- main.m
+++ main.m
@@ -11,6 +11,23 @@ sessionID = input('sessionID: ', 's');
 % Here we call some default settings for setting up Psychtoolbox
 PsychDefaultSetup(2);
 
+% Initialize Vpixx
+isOpen = Datapixx('Open'); % check if Vpixx screen is connected
+if ~isOpen
+    error('VIEWPixx not connected! Please check connection and try again');
+end
+PsychDataPixx('Open');
+% switch to ScanningBacklight mode for full illumination only after pixels stabilize 
+PsychDataPixx('EnableVideoScanningBacklight'); % comment for photodiode test
+ResponsePixx('Close'); % to make sure open doesn't fail
+ResponsePixx('Open');
+Datapixx('EnableDinDebounce');
+Datapixx('RegWr');
+
+doutValue = bin2dec('0000 0000 0000 0000 0000 0000'); % initialize digital output
+Datapixx('SetDoutValues', doutValue);
+Datapixx('RegWr');
+
 % Get the screen numbers
 screens = Screen('Screens');
 
@@ -26,4 +43,15 @@ session = initSession(subjectID, sessionID, 5, 72, 72, 10, w, windowRect);
 
 session = runSession(session);
 
+% save session
+fileName = sprintf('..%cdata%cMasking_Sub_%d',filesep,filesep,session.subjectID);
+try
+    save(fileName,'session');
+catch
+    mkdir(sprintf('..%cdata',filesep));
+    save(fileName,'session');
+end
+
 sca;
+ResponsePixx('Close');
+Datapixx('Close');
